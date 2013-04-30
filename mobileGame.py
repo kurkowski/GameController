b171500ab9ec0e3e2c9e307b56b0230b27c07319
jae <jaegawarui@gmail.com>
2013-04-30 04:58:15 -0500

    use users api to track person

--- mobileGame.py
+++ mobileGame.py
@@ -1,20 +1,40 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.api import users
 
 JINJA_ENVIRONMENT = jinja2.Environment(
 	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+
+def getUser(self):
+	user = users.get_current_user()
+	if user:
+		return user.nickname()
+	else:
+		self.redirect(users.create_login_url(self.request.uri))
+
+
 class MainPage(webapp2.RequestHandler):
 	def get(self):
+		user_nickname = getUser(self)		
 		template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+		template_values = {'name':user_nickname}
+		self.response.write(template.render(template_values))
+
+class CreateHandler(webapp2.RequestHandler):
+	def get(self):
+		user_nickname = users.getUser()
+		room = Room()
+		template = JINJA_ENVIRONMENT.get_template('templates/create.html')
 		template_values = {}
 		self.response.write(template.render(template_values))
 
 class ConnectHandler(webapp2.RequestHandler):
 	def get(self):
+		user_nickname = users.getUser()
 		code = self.request.get('roomCode')
-		#todo: first need to create room before implementing join
+		#todo: first need to create room before implementing connect
 
 
 		template = JINJA_ENVIRONMENT.get_template('templates/connect.html')
@@ -51,6 +71,7 @@ class ControlsTestHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
 	webapp2.Route(r'/', handler=MainPage, name='main'),
+	webapp2.Route(r'/room/create', handler=CreateHandler, name='create'),
 	webapp2.Route(r'/room/connect', handler=ConnectHandler, name='connect'),
 	webapp2.Route(r'/game/test/controls', handler=ControlsTestHandler, name='controls_test'),
 ], debug=True)
