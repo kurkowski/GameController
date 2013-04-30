b5d2522e5a99e290492ee1dcea83761021b26011
kurkowski <queso8989@hotmail.com>
2013-04-30 07:02:51 -0500

    Updated client-server communication, game displays sequence from server

--- mobileGame.py
+++ mobileGame.py
@@ -1,13 +1,17 @@
 import webapp2
 import jinja2
 import os
+import json
+import random
+import logging
+from google.appengine.api import memcache
 
 JINJA_ENVIRONMENT = jinja2.Environment(
 	loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('index.html')
+		template = JINJA_ENVIRONMENT.get_template('gameScreen.html')
 		template_values = {}
 		self.response.write(template.render(template_values))
 
@@ -21,14 +25,15 @@ class ConnectHandler(webapp2.RequestHandler):
 		template_values = {'code':code,}
 		self.response.write(template.render(template_values))
 
+numToLetter = ['a', 'b', 'd', 'l', 'r', 'u']
 
 #temporary code to test controller
 class ControlsTestHandler(webapp2.RequestHandler):
 	def post(self):
-		button_letter = self.request.post('button')
+		button_letter = self.request.get('button')
 		# initialize to 'none'
 		memcache.add(key='letter', value='none', time=3600)
-		update_letter('letter', button_letter)
+		self.update_letter('letter', button_letter)
 
 	def get(self):
 		letter = memcache.get('letter')
@@ -39,18 +44,39 @@ class ControlsTestHandler(webapp2.RequestHandler):
 		template_values = {'letter':letter,}
 		self.response.write(template.render(template_values))
 
-
-	def update_letter(key, value):
+	def update_letter(self, key, value):
 	   client = memcache.Client()
 	   while True:
 	     counter = client.gets(key)
+	     sequence = client.gets("sequence")
+	     logging.info(counter)
+	     logging.info(sequence)
 	     assert counter is not None, 'Uninitialized counter'
 	     if client.cas(key, value):
+		if counter == numToLetter[int(sequence)]:
+			self.response.write(json.dumps({"status": "true"}))
+		else:
+			self.response.write(json.dumps({"status": "false"}))
 	        break
 
+class UpdateSequence(webapp2.RequestHandler):
+	def get(self):
+		num = self.request.get('num')
+		memcache.add(key='sequence', value=num, time=2)
+
+class GenerateRandomButtons(webapp2.RequestHandler):
+	def get(self):
+		data = {'array': []}
+		for x in range(0, 200):
+			data['array'].append(random.randint(0,5))
+		jsonObj = json.dumps(data)
+		self.response.write(jsonObj)
+		memcache.add(key="sequence", value="none", time=2)
 
 app = webapp2.WSGIApplication([
 	webapp2.Route(r'/', handler=MainPage, name='main'),
 	webapp2.Route(r'/room/connect', handler=ConnectHandler, name='connect'),
 	webapp2.Route(r'/game/test/controls', handler=ControlsTestHandler, name='controls_test'),
-], debug=True)
+	webapp2.Route(r'/game/test/sequence', handler=GenerateRandomButtons, name='sequence'),
+	webapp2.Route(r'/game/test/update', handler=UpdateSequence, name='update')
+	], debug=True)
