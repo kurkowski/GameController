eb6b3888b548f50b8e8a86f103bd9d4e42715fe1
kurkowski <queso8989@hotmail.com>
2013-04-30 07:01:28 -0500

    Added in css for controller

--- js/standard.js
+++ js/standard.js
@@ -1,16 +1,71 @@
+var count=0;
+var images = ['abutton.png', 'bbutton.png', 'downarrow.png', 'leftarrow.png', 'rightarrow.png', 'uparrow.png'];
+
+function getSequence(){
+      $.ajax({
+      type: 'GET',
+      url: 'game/test/sequence',
+      timeout: '10000',
+      success: function(d)
+      {
+	 var data = JSON.parse(d);
+         window.setInterval(function(){
+		if (count < data.array.length){
+			var display = $("#imageBox");
+			var num = data.array[count];
+			count++;
+			display.attr('src', 'css/'+images[num]);
+			display.attr('value', num);
+			updateSequence(num);
+
+		}
+	}, 2000);
+      },
+      error: function(request, status, err) {
+         console.log(request);
+	 console.log(status);
+         console.log(err);			
+      } 
+      });
+}
+
+function updateSequence(num){
+	 var dataObject = {
+		"num": num
+	 };
+
+	 $.ajax({
+      		type: 'GET',
+      		url: 'game/test/update',
+      		timeout: '10000',
+	 	data: dataObject,
+	 	success: function(data){
+			
+		},
+	 	error: function(request, status, err) {
+         		console.log(request);
+	 		console.log(status);
+         		console.log(err);			
+      		} 
+	 });
+}
+
+var balloonSize = 0;
 
 function sendButtonPress(letter){
+   var buttonInSequence = images[$("#imageBox").attr('value')].charAt(0);
    var dataObject = {
-      "button": letter
+      "button": letter,
+      "sequence": buttonInSequence
    }
    $.ajax({
       type: 'POST',
-      url: 'something',
+      url: 'game/test/controls',
       data: dataObject,
       timeout: '10000',
       success: function(d)
       {
-         console.log('success'); 
+         console.log(d);
       },
       error: function(request, status, err) {
          console.log(request);
@@ -19,3 +74,5 @@ function sendButtonPress(letter){
       } 
    });
 }
+
+
