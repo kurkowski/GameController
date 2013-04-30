ebcf0cd9d5ef252ee4eaa6afc176a42257d6bc8c
jae <jaegawarui@gmail.com>
2013-04-30 08:32:31 -0500

    added player model to keep track of in/out room

--- mobileGame.py
+++ mobileGame.py
@@ -1,7 +1,9 @@
 import webapp2
 import jinja2
 import os
+import json
 from google.appengine.api import users
+from google.appengine.api import memcache
 from models import *
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -11,37 +13,70 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 def getUser(self):
 	user = users.get_current_user()
 	if user:
+		player = Player.query_player(user_key(user.nickname())).fetch(1)
+		if not player:
+			player = Player(parent=user_key(user.nickname()))
+			player.put()
 		return user.nickname()
 	else:
 		self.redirect(users.create_login_url(self.request.uri))
 
+def user_key(user_nickname):
+	return ndb.Key('User',user_nickname or 'NotLoggedIn')
+
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		user_nickname = getUser(self)		
+		user_nickname = getUser(self)	
+		player = Player.query_player(user_key(user_nickname)).fetch(1)
 		template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-		template_values = {'name':user_nickname}
+		template_values = {'name':user_nickname, 'inRoom':player}
 		self.response.write(template.render(template_values))
 
 class CreateHandler(webapp2.RequestHandler):
 	def get(self):
 		user_nickname = getUser(self)
-		room = Room()
+		player = Player.query_player(user_key(user_nickname)).fetch(1)[0]
+		room = Room(parent = user_key(user_nickname))
 		room.creator = user_nickname
+		room.status = 1
+		room.players = [user_nickname]
+		player.in_room = True
 		room.put()
 		template = JINJA_ENVIRONMENT.get_template('templates/create.html')
-		template_values = {'creator':room.creator, 'date':room.date}
+		template_values = {'creator':room.creator, 'date':room.date, 'players':room.players, 'inRoom':player.in_room}
 		self.response.write(template.render(template_values))
 
 class ConnectHandler(webapp2.RequestHandler):
 	def get(self):
 		user_nickname = getUser(self)
-		code = self.request.get('roomCode')
-		#todo: first need to create room before implementing connect
+		player = Player.query_player(user_key(user_nickname))
+		creator_nickname = self.request.get('creatorNickname')
 
+		room = Room.query_room(user_key(creator_nickname)).fetch(1)[0]
+		if room:
+			if player.in_room:
+				pass
+				#connect to room he is in
+			else:
+				room = room[0]
+				status = room.status
+				date = room.date
+				room.players.append(user_nickname)
+				players = room.players
+				player.in_room = True
+				player.room_owner = creator_nickname
+				room.put()
+		else:
+			status = -1
+			date = -1
+			players = []
 
 		template = JINJA_ENVIRONMENT.get_template('templates/connect.html')
-		template_values = {'code':code,}
+		template_values = {'creatorNickname':creator_nickname, 
+							'status':status, 
+							'date': date, 
+							'players': players,}
 		self.response.write(template.render(template_values))
 
 
--- models.py
+++ models.py
@@ -1,22 +1,35 @@
 from google.appengine.ext import ndb
 
 
-class User(ndb.Model):
-	email = ndb.StringProperty(indexed=False)
+#class User(ndb.Model):
+#	email = ndb.StringProperty(indexed=False)
+#	status = ndb.IntegerProperty(indexed=False)
+#	session_key = ndb.IntegerProperty(indexed=False)
+
+#class Friend(ndb.Model):
+#	user_key1 = ndb.IntegerProperty(indexed=False)
+#	user_key2 = ndb.IntegerProperty(indexed=False)
+
+#class Game(ndb.Model):
+#    game_played = ndb.IntegerProperty(indexed=False)
+#    winner = ndb.IntegerProperty(indexed=False)
+#    session_key = ndb.IntegerProperty(indexed=False)
+
+class Room(ndb.Model):
+	creator = ndb.StringProperty(indexed=False)
 	status = ndb.IntegerProperty(indexed=False)
-	session_key = ndb.IntegerProperty(indexed=False)
-
-class Friend(ndb.Model):
-	user_key1 = ndb.IntegerProperty(indexed=False)
-	user_key2 = ndb.IntegerProperty(indexed=False)
-
-class Game(ndb.Model):
-    game_played = ndb.IntegerProperty(indexed=False)
-    winner = ndb.IntegerProperty(indexed=False)
-    session_key = ndb.IntegerProperty(indexed=False)
-
-class Session(ndb.Model):
-	creator = ndb.IntegerProperty(indexed=False)
-	status = ndb.IntegerProperty(indexed=False)
-
+	limit = ndb.IntegerProperty(indexed=False)
+	players = ndb.StringProperty(indexed=False, repeated=True)
+	date = ndb.DateTimeProperty(auto_now_add=True)
     
+	@classmethod
+	def query_room(cls, ancestor_key):
+		return cls.query(ancestor=ancestor_key).order(-cls.date)
+
+class Player(ndb.Model):
+	in_room = ndb.BooleanProperty(indexed=False, default=False)
+	room_owner = ndb.StringProperty(indexed=False)
+
+	@classmethod
+	def query_player(cls, ancestor_key):
+		return cls.query(ancestor=ancestor_key)
